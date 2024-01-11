982fb436f8aecab74c6c42e7da16e30543abb15a
taro710 <katayamayutaro@team-lab.com>
2024-01-11 22:44:01 +0900

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pwa-react-experiment/", // for github pages
+  base: "/", // for github pages
   build: {
     outDir: "docs",
   },
